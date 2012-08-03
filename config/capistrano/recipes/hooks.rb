530a3cff059c8cff78dbebf994ff891ac88d9bfc
James Zhan <zhiqiangzhan@gmail.com>
2012-08-03 16:48:24 +0800

    Modify the deploy scripts

--- config/capistrano/recipes/hooks.rb
+++ config/capistrano/recipes/hooks.rb
@@ -1,9 +1,11 @@
 # Common hooks for all scenarios.
 Capistrano::Configuration.instance.load do
   after 'deploy:setup' do
-    run "export"
     app.setup
     bundler.setup if Capistrano::CLI.ui.agree("Do you need to install the bundler gem? [Yn]")
+    
+    upload File.expand_path("../../../config/auth_providers.yml", __FILE__),  "#{shared_path}/config/auth_providers.yml"
+    upload File.expand_path("../../../config/newrelic.yml", __FILE__), "#{shared_path}/config/newrelic.yml"
   end
     
   after "deploy:update_code" do
