fce27ee0d7df64811ad5ab173d8c800bba2b9b2e
James Zhan <zhiqiangzhan@gmail.com>
2012-08-03 16:13:53 +0800

    Add auth_providers.yml

--- config/capistrano/recipes/unicorn.rb
+++ config/capistrano/recipes/unicorn.rb
@@ -69,6 +69,7 @@ Capistrano::Configuration.instance.load do
     task :setup, :roles => :app , :except => { :no_release => true } do
       # TODO: refactor this to a more generic setup task once we have more socket tasks.
       run "mkdir -p #{sockets_path}"
+      run "mkdir -p #{deploy_to}/shared/log/"
       run "chown #{user}:#{group} #{sockets_path}"
       run "chmod +rw #{sockets_path}"
 #      sudo "mkdir -p #{sockets_path}"
