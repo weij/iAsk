d8b3a74c41989c22943d2f129c62d4d1b5c77431
James Zhan <zhiqiangzhan@gmail.com>
2012-08-03 16:54:30 +0800

    revert.

--- config/capistrano/recipes/hooks.rb
+++ config/capistrano/recipes/hooks.rb
@@ -3,9 +3,6 @@ Capistrano::Configuration.instance.load do
   after 'deploy:setup' do
     app.setup
     bundler.setup if Capistrano::CLI.ui.agree("Do you need to install the bundler gem? [Yn]")
-    
-    upload File.expand_path("../../../config/auth_providers.yml", __FILE__),  "#{shared_path}/config/auth_providers.yml"
-    upload File.expand_path("../../../config/newrelic.yml", __FILE__), "#{shared_path}/config/newrelic.yml"
   end
     
   after "deploy:update_code" do
