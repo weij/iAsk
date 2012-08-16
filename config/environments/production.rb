32b5d0d5147c8a203940322794fb596bf52284c2
James Zhan <zhiqiangzhan@gmail.com>
2012-08-16 10:29:36 +0800

    test the redis cache affects.

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -26,7 +26,8 @@ Shapado::Application.configure do
 
   # Use a different cache store in production
   # config.cache_store = :mem_cache_store
-  config.cache_store = :redis_store,  {:expires_in => 2.hours}
+#TODO  
+  config.cache_store = :redis_store,  {:expires_in => 1.minutes}
   #config.cache_store = :redis_store, {:expires_in => 2.hours}
 
   #config.cache_store = [:file_store, "#{Rails.root}/tmp/cache"]
