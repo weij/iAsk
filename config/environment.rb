7ee96109334debdcb8bf1ceab2f27bdb57652d2d
Feo <jjzxcc@gmail.com>
2012-08-22 16:52:50 +0800

    Fix the wrong remove in environment.rb

--- config/environment.rb
+++ config/environment.rb
@@ -1,5 +1,5 @@
 # Load the rails application
 require File.expand_path('../application', __FILE__)
 
-puts "xxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxx"
+# Initialize the rails application
 Shapado::Application.initialize!
