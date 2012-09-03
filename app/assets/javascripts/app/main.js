e7c020763f3fc96f6b630462d088c08895177f34
James Zhan <zhiqiangzhan@gmail.com>
2012-09-03 10:46:16 +0800

    Fix IE not support HTML5 placeholder issue.

--- app/assets/javascripts/app/main.js
+++ app/assets/javascripts/app/main.js
@@ -2,5 +2,22 @@
 $(document).ready(function() {
   var $body = $(document.body);
 	$('article:even').addClass("article_even");
+	
+	if (!("placeholder" in document.createElement("input"))) {
+  	$('input[placeholder]').focus(function() {
+      var input = $(this);
+      if (input.val() == input.attr('placeholder')) {
+        input.val('');
+        input.removeClass('placeholder');
+      }
+    }).blur(function() {
+      var input = $(this);
+      if (input.val() == '' || input.val() == input.attr('placeholder')) {
+        input.addClass('placeholder');
+        input.val(input.attr('placeholder'));
+      }
+    }).blur();
+  }
+  
   Loader.initialize($body, true);
 });
