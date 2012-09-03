81e9286b301e5c948e170ca9f5e9a78a65cd05da
James Zhan <zhiqiangzhan@gmail.com>
2012-09-03 17:48:56 +0800

    Fix IE placeholder password bug.

--- app/assets/javascripts/app/main.js
+++ app/assets/javascripts/app/main.js
@@ -3,28 +3,40 @@ $(document).ready(function() {
   var $body = $(document.body);
 	$('article:even').addClass("article_even");
 	
-	if (!("placeholder" in document.createElement("input"))) {
-  	$('input[placeholder]').focus(function() {
+	var supportPlaceHolder = ("placeholder" in document.createElement("input"));
+	if (!supportPlaceHolder) {
+	  $(':password[placeholder]').each(function(){
+	    var password = $(this);
+	    var fakeNode = $(this.outerHTML.replace(/type=(['"])?password\1/gi, 'type=$1text$1'));
+	    fakeNode.insertBefore(password).focus(function(){
+        password.show();
+        $(this).hide();
+      });
+      password.blur(function(){
+        if(password.val() == ''){
+          fakeNode.show();
+          $(this).hide();
+        }
+      }).blur();
+	  });
+	  
+  	$(':text[placeholder]').focus(function() {
       var input = $(this);
       if (input.val() == input.attr('placeholder')) {
         input.val('');
         input.removeClass('placeholder');
-        if (input.attr('type') == 'password') {
-          input.attr('origin_type', 'password')
-          input.attr('type', 'text')
-        }
       }
     }).blur(function() {
       var input = $(this);
       if (input.val() == '' || input.val() == input.attr('placeholder')) {
         input.addClass('placeholder');
         input.val(input.attr('placeholder'));
-        if (input.attr('origin_type') == 'password') {
-          input.attr('type', 'password')
-        }
       }
     }).blur();
-  }
-  
+	}
+	
+	$('.search_text,:password').val('');
+
   Loader.initialize($body, true);
 });
+
