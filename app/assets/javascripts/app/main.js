152625f6ed606630138c9918eeccf9691977b9b6
James Zhan <zhiqiangzhan@gmail.com>
2012-09-03 11:03:54 +0800

    Fix password issue.

--- app/assets/javascripts/app/main.js
+++ app/assets/javascripts/app/main.js
@@ -9,12 +9,19 @@ $(document).ready(function() {
       if (input.val() == input.attr('placeholder')) {
         input.val('');
         input.removeClass('placeholder');
+        if (input.attr('type') == 'password') {
+          input.attr('origin_type', 'password')
+          input.attr('type', 'text')
+        }
       }
     }).blur(function() {
       var input = $(this);
       if (input.val() == '' || input.val() == input.attr('placeholder')) {
         input.addClass('placeholder');
         input.val(input.attr('placeholder'));
+        if (input.attr('origin_type') == 'password') {
+          input.attr('type', 'password')
+        }
       }
     }).blur();
   }
