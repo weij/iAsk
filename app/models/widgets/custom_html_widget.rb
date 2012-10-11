9054d5750ba872c7d6ecb52d3a51eaeffcf15cd9
James Zhan <zhiqiangzhan@gmail.com>
2012-10-11 11:44:19 +0800

    Add Customize Widgets.

--- app/models/widgets/custom_html_widget.rb
+++ app/models/widgets/custom_html_widget.rb
@@ -1,5 +1,10 @@
 class CustomHtmlWidget < Widget
-  field :settings, :type => Hash, :default => {'content' => {}}
+  field :settings, :type => Hash, :default => {'content' => {}, 'title' => {}}
+
+  def title
+    settings['title'][I18n.locale.to_s.split("-").first] ||
+    settings['title'][self.group.language] || ""
+  end
 
   def content
     settings['content'][I18n.locale.to_s.split("-").first] ||
